ba41d4794c3e67b0494ade189c3c7ad08e2533bb
Nilesh <alokbabar7249@gmail.com>
2026-01-24 14:55:29 +0530

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all new js code as newer version
+
+//alert(4 + 4)// we are using node js not a browser
+
+console.log(4+4)
+
+console.log("Nilesh")
+
+let name= "Nilesh"
+let age= "21"
+let isloggedIN= true
+let state;
+
+//Number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null=> standalone value
+//undefind =>
+// symbol =>unique
+
+
+//object
+
+console.log(typeof undefined); //undefinde
+console.log(typeof null); //object
